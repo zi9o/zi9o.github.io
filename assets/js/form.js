db2e6ce5815a2db142feb0b3edbde20fe02e23f2
zi9o <zakariaelfakhry@gmail.com>
2018-12-12 14:26:52 +0100

    add contact form page

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,24 +1,25 @@
-$(document).ready(function() {
-	
-	    
-    /* ======= jQuery form validator ======= */ 
-    /* Ref: http://jqueryvalidation.org/documentation/ */   
-    $("#contact-form").validate({
-		messages: {
-		
-		    name: {
-    			required: 'Please enter your name' //You can customise this message
-			},
-			email: {
-				required: 'Please enter your email' //You can customise this message
-			},			
-			message: {
-				required: 'Please enter your message' //You can customise this message
-			}
-			
-		}
-		
-	});
-    
+$(document).ready(function () {
+
+
+  /* ======= jQuery form validator ======= */
+  /* Ref: http://jqueryvalidation.org/documentation/ */
+  $('#contact-form').validate({
+    messages: {
+
+      name: {
+        required: 'Please enter your name' //You can customise this message
+      },
+      email: {
+        required: 'Please enter your email' //You can customise this message
+      },
+      message: {
+        required: 'Please enter your message', //You can customise this message
+        minlength: 'Please enter your message with minimum of 50 characters'
+      }
+
+    }
+
+  });
+
 
 });
